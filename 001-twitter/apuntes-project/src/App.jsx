7af59edf6d6a187d29b9c90c427a721e27bc088b
AnaLR27 <analorenzo.2@hotmail.com>
2025-02-20 14:41:26 +0100

    .

---
+++ 001-twitter/apuntes-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Button } from "./components/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button texto="Botón de prueba"></Button>
+    </>
+  );
+}
+
+export default App;
---
+++ 001-twitter/apuntes-project/src/components/Button.jsx
@@ -0,0 +1,3 @@
+export const Button = ({ texto }) => {
+  return <button> {texto}</button>;
+};
